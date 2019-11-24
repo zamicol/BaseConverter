2bf0743ffa4217cf20d9b7ca64a219226f604955
Zamicol <zamicol@gmail.com>
2019-11-24 02:15:33 -0700

    add length

--- app.js
+++ app.js
@@ -4,11 +4,29 @@ document.addEventListener('DOMContentLoaded', () => {
 	document.querySelector("#ConvertBaseButton").addEventListener('click', Convert);
 	document.querySelector("#ExampleButton").addEventListener('click', ExampleConversion);
 	document.querySelector("#FlipButton").addEventListener('click', Flip);
-	document.querySelectorAll("#ClearButton").forEach(t => {t.addEventListener('click', Clear);});
-	document.querySelectorAll(".copy").forEach(t => {t.addEventListener('click', Copy);});
-	document.querySelectorAll(".source").forEach(t => {t.addEventListener('click', Source);});
-	document.querySelectorAll(".destination").forEach(t => {t.addEventListener('click', Desintation);});
-	
+	document.querySelectorAll("#ClearButton").forEach(t => {
+		t.addEventListener('click', Clear);
+	});
+	document.querySelectorAll(".copy").forEach(t => {
+		t.addEventListener('click', Copy);
+	});
+	document.querySelectorAll(".source").forEach(t => {
+		t.addEventListener('click', Source);
+	});
+	document.querySelectorAll(".destination").forEach(t => {
+		t.addEventListener('click', Desintation);
+	});
+
+
+
+	document.querySelectorAll(".conversionTextArea").forEach(t => {
+		t.addEventListener('input', Length);
+	});
+	// I don't know if change does anything, but input doesn't seem to cover all
+	// cases of user interaction. 
+	document.querySelectorAll(".conversionTextArea").forEach(t => {
+		t.addEventListener('change', Length);
+	});
 });
 
 
@@ -22,6 +40,7 @@ function Convert() {
 	let outputString = baseConvert(inputString, inputAlphabet, outputAlphabet);
 	console.log(outputString);
 	document.getElementById("outputString").value = outputString;
+	Update();
 }
 
 
@@ -35,6 +54,8 @@ function Flip() {
 	document.getElementById("inputString").value = outputString;
 	document.getElementById("outputAlphabet").value = inputAlphabet;
 	document.getElementById("outputString").value = inputString;
+
+	Update();
 }
 
 
@@ -43,38 +64,64 @@ function ExampleConversion() {
 	document.getElementById("inputAlphabet").value = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 	document.getElementById("outputAlphabet").value = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-";
 	Convert();
+	Update();
 }
 
 
-function Copy(){
-console.log("Entered Copy");
-// Alternative selector:
-//console.log(this.parentNode.previousElementSibling.firstElementChild.value);
+function Copy() {
+	//console.log("Triggered Copy");
+	// Alternative selector:
+	//console.log(this.parentNode.previousElementSibling.firstElementChild.value);
 
-let input = this.parentNode.previousElementSibling.firstElementChild;
-input.select();
-input.setSelectionRange(0, 99999);
-document.execCommand("copy");
+	let input = this.parentNode.previousElementSibling.firstElementChild;
+	input.select();
+	input.setSelectionRange(0, 99999);
+	document.execCommand("copy");
 }
 
-function Source(){
+function Source() {
 	console.log("Entered Source");
 	let input = this.parentNode.parentNode.firstElementChild.firstElementChild.value;
-	console.log(input);
-	document.getElementById("inputAlphabet").value =input;
+	document.getElementById("inputAlphabet").value = input;
+	Update();
 }
 
-function Desintation(){
+function Desintation() {
 	console.log("Entered Desintation");
 	let input = this.parentNode.parentNode.firstElementChild.firstElementChild.value;
-	console.log(input);
 	document.getElementById("outputAlphabet").value = input;
+	Update();
 }
 
-function Clear(){
-	console.log("Entered Clear");
+function Clear() {
+	//console.log("Triggered Clear");
 	document.getElementById("inputAlphabet").value = "";
 	document.getElementById("inputString").value = "";
 	document.getElementById("outputAlphabet").value = "";
 	document.getElementById("outputString").value = "";
+	Update();
+}
+
+
+function Length() {
+	//console.log("Triggered Length");
+	//console.log(this);
+	this.parentNode.querySelector(".lengthDisplay").textContent = this.value.length;
+}
+
+
+function LengthAll() {
+	//console.log("Triggered LengthAll");
+
+	document.getElementById("inputAlphabetLength").textContent = document.getElementById("inputAlphabet").value.length;
+	document.getElementById("inputStringLength").textContent = document.getElementById("inputString").value.length;
+	document.getElementById("outputAlphabetLength").textContent = document.getElementById("outputAlphabet").value.length;
+	document.getElementById("outputStringLength").textContent = document.getElementById("outputString").value.length;
+
+
+}
+
+// Update updates all information presented on the screen.  
+function Update(){
+	LengthAll();
 }
